ebdab3c175bc19f3e673d354a07ac7d1562a6871
ameba23 <ameba23@systemli.org>
2019-12-14 00:17:55 +0100

    settings view

---
+++ views/settings.js
@@ -0,0 +1,28 @@
+const html = require('choo/html')
+const request = require('../request')
+const { formData } = require('../util')
+const basic = require('./basic')
+module.exports = view
+
+function view (state, emit) {
+  return basic(html`
+    <h3>Settings</h3>
+    <p>Current name: ${JSON.stringify(state.settings.peerNames)}</p>
+    <form id="addname" onsubmit=${onSubmit}>
+      <label for="name">Name:</label>
+      <input type=text id="name" value="" name="name">
+      <input type=submit value="Update name">
+    </form>
+  `)
+
+  function onSubmit (e) {
+    e.preventDefault()
+    var form = e.currentTarget
+    var thing = formData(form)
+    request.post('/settings', thing)
+      .then((res) => {
+        emit('updateSettings', res)
+      })
+      .catch(console.log) // TODO
+  }
+}
