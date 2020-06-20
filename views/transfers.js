c4a923c01058576507b54a05786a94c6b1c1bfdb
ameba23 <ameba23@systemli.org>
2020-06-20 18:54:34 +0200

    transfers screen

--- views/transfers.js
+++ views/transfers.js
@@ -1,16 +1,18 @@
 const html = require('choo/html')
+const h = require('hyperscript')
 const TITLE = 'metadb - transfers'
 const basic = require('./basic')
-const h = require('hyperscript')
+const createRequest = require('../request')
 
 module.exports = view
 
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+  const request = createRequest(state.connectionSettings)
 
   return basic(state, emit, html`
-    <h3>Requests sent:</h3>
-    <ul>${state.request.map(displayRequest)}</ul>
+    <h3>Wish list:</h3>
+    <ul>${state.request.map(displayWishListItem)}</ul>
     <h3>Current downloads:</h3>
     <ul>${state.wsEvents.download
     ? Object.keys(state.wsEvents.download).map(displayDownloadingFile)
@@ -23,16 +25,29 @@ function view (state, emit) {
   // function displayTimestamp (timestamp) {
   //   return new Date(timestamp).toLocaleString()
   // }
-  //
-  // function displayPeer (feedId) {
-  //   const peer = state.peers.find(p => p.feedId === feedId)
-  //   if (!peer) return feedId
-  //   return h('a', { href: `#peers/${peer.feedId}` }, peer.name || peer.feedId)
-  // }
 
-  function displayRequest (request) {
+  function displayPeer (feedId) {
+    const peer = state.peers.find(p => p.feedId === feedId)
+    if (!peer) return feedId
+    return h('a', { href: `#peers/${peer.feedId}` }, peer.name || peer.feedId)
+  }
+
+  function displayWishListItem (file) {
     // TODO
-    return h('li', request.filename.toString())
+    return h('li',
+      h('a', { href: `#files/${file.sha256}` }, file.filename.toString()),
+      ' held by: ', file.holders.map(displayPeer),
+      h('button', { onclick: unrequest(file.sha256) }, 'Remove from wishlist')
+    )
+  }
+
+  function unrequest (files) {
+    if (!Array.isArray(files)) files = [files]
+    request.delete('/request', { files })
+      .then((res) => {
+        emit('updateConnection', res)
+      })
+      .catch(console.log) // TODO
   }
 
   function displayDownloadingFile (name) {
