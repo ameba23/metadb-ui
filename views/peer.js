c0c86ca661eb9568729fcd32cf0be1002ef14856
ameba23 <ameba23@systemli.org>
2021-03-17 21:49:39 +0100

    bug with non existing peer

--- views/peer.js
+++ views/peer.js
@@ -1,17 +1,17 @@
 const h = require('hyperscript')
-const TITLE = 'metadb - peers'
 const basic = require('./basic')
 const { filesView } = require('./files')
 const { createDisplayPeer } = require('../components')
+const TITLE = 'metadb - peer'
 
 module.exports = view
 
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
   const peer = state.peers.find(peer => peer.feedId === state.params.peerId)
-  peer.stars = peer.stars || 0
   if (peer) {
     peer.name = peer.name || peer.feedId
+    peer.stars = peer.stars || 0
     return basic(state, emit,
       h('div',
         h('h3', createDisplayPeer(state, { short: true })(peer)),
